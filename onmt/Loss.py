e066085c9958742a68e67b9fa59e35e4bfcd7c0d
Qchan233 <Moeyfishyq@outlook.com>
2018-04-22 14:59:28 +0800

    Update Loss.py

--- onmt/Loss.py
+++ onmt/Loss.py
@@ -149,11 +149,11 @@ class LossComputeBase(nn.Module):
     def _unbottle(self, v, batch_size):
         return v.view(-1, batch_size, v.size(1))
       
-class DDPGLossCompute1(LossComputeBase):
+class DDPGLossComputeCritic(LossComputeBase):
 
     """
 
-    Standard NMT Loss Computation.
+    Standard NMT Loss Computation for Critic
 
     """
 
@@ -162,13 +162,33 @@ class DDPGLossCompute1(LossComputeBase):
         super(DDPGLossCompute, self).__init__(generator, tgt_vocab)
 
         self.criterion = nn.MSELoss(size_average=True)
+        self.ys = ys
         self.values_optim = values_optim
         self.values_fit = values_fit
         
     def _compute_loss(self):
-        loss = self.criterion(ys, self.values_optim)
+        loss = self.criterion(self.ys, self.values_fit)
         return loss
+      
+class DDPGLossComputeActor(LossComputeBase):
 
+    """
+
+    Standard NMT Loss Computation for actor
+
+    """
+
+    def __init__(self, normalization="sents", ys, values_fit, values_optim):
+
+        super(DDPGLossCompute, self).__init__(generator, tgt_vocab)
+
+        self.values_optim = values_optim
+        self.values_fit = values_fit
+        
+    def _compute_loss(self):
+        loss = -torch.mean(values_optim)
+        return loss
+      
 class NMTLossCompute(LossComputeBase):
     """
     Standard NMT Loss Computation.
